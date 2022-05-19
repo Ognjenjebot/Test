0ac0269bf96331131faf75ec9ebf05923323d7ca
Pavle Tenjovic <pavle.tenja@gmail.com>
2022-05-19 03:23:13 +0200

    proba7

--- proba.c
+++ proba.c
@@ -1,13 +1,13 @@
 #include <iostream>
 
-<<<<<<< HEAD
+
 int nesto(int s, int f, int h)
 {
 	return s * f * h;
-=======
+}
 int rez(int a, int b){
 	return a+b;
->>>>>>> f267e066add1001bf08fddcefc0653745e8179f7
+
 }
 
 int main()
@@ -22,10 +22,10 @@ int main()
 
 	// tenja kuca kod ovde
 	print("asdasdadasdasd");
-<<<<<<< HEAD
+
 	int x = nesto(1, 2, 3)
-=======
+	printf("ogi je gej");
 	int p =rez(1,2);
->>>>>>> f267e066add1001bf08fddcefc0653745e8179f7
+
 	// do ovde
 }
