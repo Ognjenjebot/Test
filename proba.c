a00cf02678dc299046860792b6fde0313e54c544
Ognjen Stanojevic <ognjen.stanojevic321@gmail.com>
2022-05-19 03:24:40 +0200

    proba7

--- proba.c
+++ proba.c
@@ -1,31 +1,26 @@
 #include <iostream>
 
-<<<<<<< HEAD
 int nesto(int s, int f, int h)
 {
 	return s * f * h;
-=======
-int rez(int a, int b){
-	return a+b;
->>>>>>> f267e066add1001bf08fddcefc0653745e8179f7
-}
-
-int main()
-{
-	int i;
-	// ja cu ovde kod kucat
-	for (i = 0; i < 10; i++)
+	int rez(int a, int b)
 	{
-		printf("te321321321ina");
+		return a + b;
 	}
-	// do ovde
 
-	// tenja kuca kod ovde
-	print("asdasdadasdasd");
-<<<<<<< HEAD
-	int x = nesto(1, 2, 3)
-=======
-	int p =rez(1,2);
->>>>>>> f267e066add1001bf08fddcefc0653745e8179f7
-	// do ovde
-}
+	int main()
+	{
+		int i;
+		// ja cu ovde kod kucat
+		for (i = 0; i < 10; i++)
+		{
+			printf("te321321321ina");
+		}
+		// do ovde
+
+		// tenja kuca kod ovde
+		print("asdasdadasdasd");
+		int x = nesto(1, 2, 3) int p = rez(1, 2);
+		// do ovde
+		printf("nesto");
+	}
