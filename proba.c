e333f1827b47e1651913b777d75a6fa1e01e77b5
Ognjen Stanojevic <ognjen.stanojevic321@gmail.com>
2022-05-19 03:08:44 +0200

    proba3

--- proba.c
+++ proba.c
@@ -6,14 +6,16 @@ int main()
 	// ja cu ovde kod kucat
 	for (i = 0; i < 10; i++)
 	{
-		printf("tenja pedercina");
+		printf("te321321321ina");
 	}
 	// do ovde
 
 	// tenja kuca kod ovde
 	for (i = 0; i < 10; i++)
 	{
-		printf("Tenja je pedercina je pedercina!");
+		printf("T123421321");
+		printf("Ogi je isto peder");
+		printf("Ogi je isto peder");
 		printf("Ogi je isto peder");
 	}
 	// do ovde
